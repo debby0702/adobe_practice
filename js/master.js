6f34dcebaa6927e664b8cc748b54d4c8a154be83
Debby Yang <dla070284@gmail.com>
2024-06-24 20:17:27 +0800

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -1,7 +1,17 @@
 const $grid= $('#Grid');
 const $nav = $('#Nav');
 
-// 方法一：表達式-----------------------------------
+
+// 方法一---------------------------------
+// let isMobile;
+// if($(window).width()<=480){
+//     isMobile=true
+// }else{isMobile=false}
+// console.log(isMobile)
+
+// --------------------------------------------
+
+// 方法二：表達式-----------------------------------
 // 判斷是否是手機版(F12看console會出現true/ flase)
 const isMobile = $(window).width()<=480;
 
@@ -12,31 +22,21 @@ console.log(isMobile)
 // console.log($(window).height())
 
 
-// 方法二---------------------------------
-// let isMobile;
-// if($(window).width()<=480){
-//     isMobile=true
-// }else{isMobile=false}
-// console.log(isMobile)
-
-// --------------------------------------------
-
-
 function setGrid(){
     // 這裡要用2個or3個=
     // if (isMobile==true){
     // 但其實不用寫==true(優化)
-    if (isMobile){
-        $grid.addClass('grid-2')
-    }else{
-        $grid.addClass('grid-3')
-    }
+    // if (isMobile){
+    //     $grid.addClass('grid-2')
+    // }else{
+    //     $grid.addClass('grid-3')
+    // }
 
     // 優化寫法(純粹減少大括號)
     if (isMobile){
         $grid.addClass('grid-2')
         return;
-        // 停下的意思
+        // 停下的意思，不加做grid-3
     }
     $grid.addClass('grid-3')
 }
@@ -45,16 +45,45 @@ function setGrid(){
 setGrid();
 
 
-// 捲動，nav就加背景
-$(window).scroll(function(){
-    // console.log(777)
-    // 內容超出可視範圍的數值
-    // console.log($(window).scrollTop());
-    if($(window).scrollTop() == 0){
-        // console.log(0)
-        $nav.removeClass('nav-active')
-    }else{
-        // console.log('要套用class')
-        $nav.addClass('nav-active')
-    }
-});
+// 捲動，nav就加背景-------------------------------------------------------
+// $(window).scroll(function(){
+//     if(isMobile){
+//         // 內容超出可視範圍的數值
+//         // console.log($(window).scrollTop());
+//         if($(this).scrollTop() == 0){
+//             // console.log(0)
+//             $nav.removeClass('nav-active')
+//         }else{
+//             // console.log('要套用class')
+//             $nav.addClass('nav-active')
+//         }
+//     }
+// });
+
+function setScroll(){
+    $(window).scroll(function(){
+        // isMobile: true
+        // !isMobile:flase
+        if(!isMobile) return;
+        
+        
+        if($(this).scrollTop() == 0){
+            // console.log(0)
+            $nav.removeClass('nav-active')
+        }else{
+            // console.log('要套用class')
+            $nav.addClass('nav-active')
+        }
+    })
+}
+
+function setInit(){
+    setGrid();
+}
+
+function setEvent(){
+    setScroll();
+}
+
+setInit();
+setEvent();
